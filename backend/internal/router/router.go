042b79886a195d5e2d95f59146857668ba4b73d8
Panshul-Jindal <panshuljindal.7@gmail.com>
2025-10-18 03:51:05 +0530

    feat: production ready??

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,28 +1,23 @@
 package router
 
 import (
-    "github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
-    r := gin.Default()
+	r := gin.Default()
 
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"*"}
 	config.AllowMethods = []string{"OPTIONS", "GET", "POST", "PUT", "DELETE"}
 	config.AllowHeaders = []string{"*"}
-	config.AllowHeaders = []string{"Content-Type"}
-	config.AllowHeaders = []string{"X-Requested-With", "Content-Type", "Accept"}
 	config.AllowCredentials = false
 	r.Use(cors.New(config))
-    // CORS middleware
-    
+	// CORS middleware
 
-  
-    // Setup routes
-    SetupRoutes(r)
+	// Setup routes
+	SetupRoutes(r)
 
-    return r
+	return r
 }
-  
