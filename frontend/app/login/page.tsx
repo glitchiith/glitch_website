7cb89ef742f83cc0fefc14b4038176d471ec8991
Panshul-Jindal <panshuljindal.7@gmail.com>
2025-10-18 03:51:05 +0530

    feat: production ready??

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "@/lib/firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
+import { apiFetch } from "@/lib/api";
 export default function LoginPage() {
   const [error, setError] = useState("");
   const router = useRouter();
@@ -30,7 +30,7 @@ export default function LoginPage() {
     setCookie("guestMode", false, { path: "/", maxAge: 60*60*24 });
 
     // 🚀 Register new user in DB if first time
-    await fetch("/api/register-user", {
+    await apiFetch("/api/register-user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
