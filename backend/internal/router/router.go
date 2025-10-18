066469e2fcdadd609fcf618e1f8934b455b9cdf8
Panshul-Jindal <panshuljindal.7@gmail.com>
2025-10-18 19:42:58 +0530

    CR fix

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -9,7 +9,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://www.glitchiith.co.in", "https://glitchiith.co.in", "http://localhost:3000", "http://127.0.0.1:3000"}
+	config.AllowOrigins = []string{"https://www.glitchiith.co.in", "https://glitchiith.co.in", "http://localhost:3000", "http://127.0.0.1:3000", "www.glitchiith.co.in", "glitchiith.co.in"}
 	config.AllowMethods = []string{"OPTIONS", "GET", "POST", "PUT", "DELETE"}
 	config.AllowHeaders = []string{"ORIGIN", "CONTENT-TYPE", "AUTHORIZATION"}
 	config.AllowCredentials = true
