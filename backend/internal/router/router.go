589210b03743cdd9ad5c35983ffb57ebc9f5ed17
glitchiith <glitch@gymkhana.iith.ac.in>
2025-10-18 07:41:51 +0000

    added access to local host

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -9,7 +9,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://www.glitchiith.co.in", "https://glitchiith.co.in", "http://localhost", "http://127.0.0.1"}
+	config.AllowOrigins = []string{"https://www.glitchiith.co.in", "https://glitchiith.co.in", "http://localhost:3000", "http://127.0.0.1:3000"}
 	config.AllowMethods = []string{"OPTIONS", "GET", "POST", "PUT", "DELETE"}
 	config.AllowHeaders = []string{"ORIGIN", "CONTENT-TYPE", "AUTHORIZATION"}
 	config.AllowCredentials = true
