7751fc21d252681fbaa66607487b9a740a6aa00b
sreeteja <gsreeteja25@gmail.com>
2026-09-13 09:56:16 +0530

    Clean up Next image configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   images: {
-    remotePatterns: [new URL('https://images.unsplash.com/*')],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
 export default nextConfig;
-
-
-
-// next.config.js
-module.exports = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-}
